02209adb9f21a77936b59a16d5cf4a949e4411f5
Adubedat <arthur.dubedat@gmail.com>
2018-04-17 19:43:02 +0200

    user input gestion done

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include "GenerateGrid.hpp"
 
-int main(int argc, char** argv){
+Grid*   getStartGrid(int const &argc, char** const &argv) {
 
-    Grid* start;
-    
     try {
-        start = GenerateGridFromFile(argv[1]);
+        if (argc < 2)
+            throw SyntaxException("Parameters are missing\nExpected format : ./n-puzzle [-size n] [file]");
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-size") == 0 && (i + 1 < argc))
+                return (GenerateRandomGrid(std::stoi(argv[++i])));
+            else
+                return (GenerateGridFromFile(argv[i]));
+        }
     } catch (std::exception &e) {
         std::cout << e.what() << std::endl;
         std::exit(EXIT_FAILURE);
     }
+    return (NULL);
+}
+
+int     main(int argc, char** argv){
+
+    Grid* start = getStartGrid(argc, argv);
     start->swap({1,0});
     start->swap({0,1});
     std::cout << start->toString() << std::endl;
